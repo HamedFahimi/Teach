26fc3d2270f21f9cbdd1f32f7f34a07a685985b6
hamedfahimi <hamedfahimi@.UM.AC.IR>
2025-04-13 09:58:52 +0330

    updated slide up to AA class

---
+++ SlideCodes/Duck4.java
@@ -0,0 +1,23 @@
+public class Duck4 {
+   private String name;  
+   private int size;
+   
+   public static void quack() {    
+     System.out.println("quack! quack!");   
+   }
+
+   public int getSize() {
+     return size;
+   }
+   public void setSize(int newSize) {
+     size = newSize;
+   }
+
+   public String getName() {
+     return name;
+   }
+   public void setName(String newName) {
+     name = newName;
+   }
+} 
+
---
+++ SlideCodes/Duck4TestDrive.java
@@ -0,0 +1,10 @@
+public class Duck4TestDrive {
+   public static void main(String[] args) {
+      Duck4 MyDuck = new Duck4();
+      MyDuck.setName("Magoo");
+      System.out.println("The name of MyDuck = " + MyDuck.getName());
+      MyDuck.setSize(42);
+      System.out.print("The size of MyDuck = " + MyDuck.getSize());
+   }
+}
+
